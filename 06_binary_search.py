c98910b8f1ea053b37f47b175cea79b77d97d4c8
pdonatilio <pdonatilio@gmail.com>
2018-08-10 10:25:39 -0300

    Fixing the PEP8 Format to binary searchg example

--- 06_binary_search.py
+++ 06_binary_search.py
@@ -1,5 +1,6 @@
 # Binary Search recursive with python
 
+
 def binary_search(array, key, start, end):
 
     if start > end:
@@ -12,18 +13,19 @@ def binary_search(array, key, start, end):
 
     # Pushing the search to the array's middle with recursivity
     if key < array[middle]:
-        return binary_search(array, key, sart, middle - 1) # To the left
-    return binary_search(array, key, middle + 1, end) # To the right
+        return binary_search(array, key, sart, middle - 1)  # To the left
+    return binary_search(array, key, middle + 1, end)  # To the right
+
 
 def binary_search_output(key):
     if binary_search(array, key, 0, len(array) - 1) == True:
-        print(key,'exists inside the array')
+        print(key, 'exists inside the array')
     else:
-        print(key,'not exists inside the array')
+        print(key, 'not exists inside the array')
 
-array = [11,5,10,20,15,4]
-array.sort() # To the binary search works, we'll need an ordered array
+array = [11, 5, 10, 20, 15, 4]
+array.sort()  # To the binary search works, we'll need an ordered array
 
-#Running the examples
+# Running the examples
 binary_search_output(20)
 binary_search_output(30)
